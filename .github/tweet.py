465894e902989f4cf8a2813b4a3ebc79cea62122
Thibault Garçon <thibault.garcon@pole-emploi.fr>
2021-11-20 16:11:52 +0100

    CHORE: github action tweet

--- .github/tweet.py
+++ .github/tweet.py
@@ -8,6 +8,10 @@ CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 
+print(CONSUMER_KEY)
+print(CONSUMER_SECRET)
+print(ACCESS_TOKEN)
+print(ACCESS_TOKEN_SECRET)
 
 twitter = Twython(
     CONSUMER_KEY,
