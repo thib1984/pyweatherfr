63deb0324f3779893083902e585f280719595160
Thibault Garçon <thibault.garcon@pole-emploi.fr>
2021-11-20 16:42:43 +0100

    CHORE: github action tweet

--- .github/tweet.py
+++ .github/tweet.py
@@ -1,5 +1,4 @@
 from twython import Twython
-# required libraries
 import os
 
 # get emails and password from environment variables
@@ -7,11 +6,6 @@ CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
-
-print(CONSUMER_KEY)
-print(CONSUMER_SECRET)
-print(ACCESS_TOKEN)
-print(ACCESS_TOKEN_SECRET)
 
 twitter = Twython(
     CONSUMER_KEY,
